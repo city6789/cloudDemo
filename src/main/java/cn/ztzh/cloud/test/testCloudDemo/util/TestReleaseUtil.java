fcedec3b2177d36777ab005ec04502d02d035408
hesy2019 <heshiyang2019@163.com>
2019-11-27 02:33:39 +0800

    release分支第一次提交

---
+++ src/main/java/cn/ztzh/cloud/test/testCloudDemo/util/TestReleaseUtil.java
@@ -0,0 +1,13 @@
+package cn.ztzh.cloud.test.testCloudDemo.util;
+/**
+ * 
+ * @author hsy02
+ *
+ */
+public class TestReleaseUtil {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Release");
+	}
+}
